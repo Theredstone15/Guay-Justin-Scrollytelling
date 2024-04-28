0a282e242cc8a5e829c72463eafbd3e6b93d0664
Justin Guay <justinguay15@gmail.com>
2024-04-27 20:41:52 -0400

    Chapitres en place

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-gsap.to(".ligne",{y:"40%", yoyo: true, repeat: -1, ease:"power1.inOut"});
+gsap.to(".ligne",{y:"60%", yoyo: true, repeat: -1, ease:"power1.inOut"});
+
+var animmenu= gsap.timeline({repeat: -1});
+animmenu.to(".Titre", {x:"1%", ease:"power1.inOut"});
+animmenu.to(".Soustitre", {x:"-1%", ease:"power1.inOut"}, "<");
+animmenu.to(".Titre", {x:"0%", ease:"power1.inOut"});
+animmenu.to(".Soustitre", {x:"0%", ease:"power1.inOut"}, "<");
+animmenu.to(".Titre", {x:"-1%", ease:"power1.inOut"});
+animmenu.to(".Soustitre", {x:"1%", ease:"power1.inOut"}, "<");
+animmenu.to(".Titre", {x:"0%", ease:"power1.inOut"});
+animmenu.to(".Soustitre", {x:"0%", ease:"power1.inOut"}, "<");
